4dd2f28f82eb227b66fb43a71ecb04ef45bb5fc7
Andrey Dyugaev <a.dugaev@yandex.ru>
2022-08-21 18:30:29 +0500

    some fixes

--- src/pages/profile/MainInfoPage.vue
+++ src/pages/profile/MainInfoPage.vue
@@ -4,7 +4,8 @@
   import { useRouter } from "vue-router";
 
   // Components
-  import { Page } from "@/components/structure";
+  import { Page, Heading } from "@/components/structure";
+  import { Card, Button } from "@/components/ui";
 
   const user = (localStorage.user) ? JSON.parse(localStorage.user) : null;
   const router = useRouter();
@@ -18,13 +19,33 @@
 
 <template>
   <Page class="page-profile">
-    <h1>
-      {{ $t('pages.profile.title') }}
-    </h1>
-    <template v-if="user?.id">
-      <h2>{{ $t('user.name') }}: {{ user.first_name }} {{ user.last_name }}</h2>
-      <p>{{ $t('user.email') }}: {{ user.email }}</p>
-      <p>{{ $t('user.phone') }}: {{ user.phone }}</p>
-    </template>
+    <Heading>
+      <h1>{{ $t('pages.profile.title') }}</h1>
+    </Heading>
+
+    <div class="flex flex-row space-x-4">
+      <Card v-if="user?.id" class="basis-1/4">
+        <template #header>
+          <h2>{{ $t('user.name') }}: {{ user.first_name }} {{ user.last_name }}</h2>
+        </template>
+        <p>{{ $t('user.email') }}: {{ user.email }}</p>
+        <p>{{ $t('user.phone') }}: {{ user.phone }}</p>
+        <template #footer>
+          <Button>Update</Button>
+        </template>
+      </Card>
+
+      <Card v-if="user?.id" class="basis-1/4">
+        <template #header>
+          <h2>Two factor authorization</h2>
+        </template>
+        <p>{{ $t('user.email') }}: {{ user.email }}</p>
+        <p>{{ $t('user.phone') }}: {{ user.phone }}</p>
+        <template #footer>
+          <Button>Update</Button>
+        </template>
+      </Card>
+    </div>
+
   </Page>
 </template>
